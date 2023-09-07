14f0ff73f5e3dfe3bb5fe36ac600497398db0cb8
sejal99 <sejal8974@gmail.com>
2023-09-07 21:33:08 +0530

    latest commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import {ToastContainer} from "react-toastify";
+import "react-toastify/dist/react-toastify.css";
+function App() {
+  return (
+    <div className="App">
+      <ToastContainer/>
+    <h1>hii</h1>
+    </div>
+  );
+}
+
+export default App;
